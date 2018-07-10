a8377298922b5b9c9a23a50ae1253c64b1b8e3cb
antdup <antoine.dupre@maxiv.lu.se>
2018-07-10 15:28:20 +0200

    Remove withe spaces

--- tango_ds/raspberry_pi/RPi.py
+++ tango_ds/raspberry_pi/RPi.py
@@ -9,18 +9,18 @@ import socket
 import sys
 
 class Raspberry:
-   
- 
+
+
     def __init__(self, host):
         self.host = host
         self.port = 9788
-        # Create a TCP socket 
+        # Create a TCP socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.settimeout(1)
-        
+
     def connect_to_pi(self):
         self.sock.connect((self.host, self.port))
-                    
+
     def str_to_bool(self, s):
         if s == 'True':
             return True
@@ -28,14 +28,14 @@ class Raspberry:
             return False
         else:
             return None
-        
+
     def query(self, cmd):
         self.sock.sendall((cmd).encode())
         val = str(self.sock.recv(1024))
         val = val.replace("'","").replace("b","")
         bol = self.str_to_bool(val)
         return bol
-        
+
     def readvoltage(self, pin):
         cmd = str(pin) + ' READVOLTAGE'
         return self.query(cmd)
@@ -43,7 +43,7 @@ class Raspberry:
     def readoutput(self, pin):
         cmd = str(pin) + ' READOUTPUT'
         return self.query(cmd)
-       
+
     def setvoltage(self, pin, value):
         cmd = str(pin) + ' SETVOLTAGE ' + str(value)
         return self.query(cmd)
@@ -51,7 +51,7 @@ class Raspberry:
     def setoutput(self, pin, value):
         data = str(pin) + ' SETOUTPUT ' + str(value)
         self.sock.sendall((data).encode())
-    
+
     def start_motor(self):
         data = 'X MOTOR True'
         self.sock.sendall((data).encode())
@@ -59,14 +59,14 @@ class Raspberry:
     def stop_motor(self):
         data = 'X MOTOR False'
         self.sock.sendall((data).encode())
-        
+
     def resetall(self):
         data = 'ALL RESET'
         self.sock.sendall((data).encode())
-        
+
     def turnoff(self):
         data = 'ALL OFF'
         self.sock.sendall((data).encode())
-        
+
     def disconnect_from_pi(self):
         self.sock.close()
